5a6b6f2361e6cbb1530c614e731e4c4eab675d00
Slaya45 <caydenfrye@gmail.com>
2023-12-05 21:04:26 -0700

    typo fix

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const ballAnswers = ["Lukas Said", "10 Steps Forward 9 Steps Back", "Go Ask Someone Else", "Do You Need a Nap?", 
 "Those Guy's!","Did You Try Turning it Off and then On?","Its Only Right after the 10th Redesign", "SG5?","Stan Is In Charge!", 
 "Just Do What They Say, Thats What Your Paid For.", "You're Not Payed to Ask Question's Just Do What They Say","Hydrogen Circuit?",
-"You'll Get That is 5-10 Buisness Months"];
+"You'll Get That in 5-10 Buisness Months"];
 
 function getRandomAnswer(arr) {
     const randomIndex = Math.floor(Math.random() * arr.length);
